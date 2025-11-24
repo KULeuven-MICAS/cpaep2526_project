291f04af28c5e47fd4ac4feacdf26f580e938e9f
rgantonio <ryan.antonio@esat.kuleuven.be>
2025-11-24 10:56:40 +0100

    hw: update gemm acc

--- rtl/gemm_accelerator_top.sv
+++ rtl/gemm_accelerator_top.sv
@@ -1,42 +1,36 @@
 // The controller for GeMM operation
 module gemm_accelerator_top #(
-    parameter int unsigned DataWidthA = 8,
-    parameter int unsigned DataWidthB = 8,
-    parameter int unsigned DataWidthC = 32,
-    parameter int unsigned SRAMAddrWidthA = 16,
-    parameter int unsigned SRAMAddrWidthB = 16,
-    parameter int unsigned SRAMAddrWidthC = 16,
-    // Calculated parameters: The Address width for the system
-    parameter int unsigned SystemAddrWidth = (SRAMAddrWidthA > SRAMAddrWidthB) ?
-                                            ((SRAMAddrWidthA > SRAMAddrWidthC) ? SRAMAddrWidthA : SRAMAddrWidthC) :
-                                            ((SRAMAddrWidthB > SRAMAddrWidthC) ? SRAMAddrWidthB : SRAMAddrWidthC)
+    parameter int unsigned InDataWidth = 8,
+    parameter int unsigned OutDataWidth = 32,
+    parameter int unsigned AddrWidth = 16,
+    parameter int unsigned SizeAddrWidth = 8
 ) (
-    input  logic                              clk_i,
-    input  logic                              rst_ni,
-    input  logic                              start_i,
-    input  logic        [SystemAddrWidth-1:0] M_size_i,
-    input  logic        [SystemAddrWidth-1:0] K_size_i,
-    input  logic        [SystemAddrWidth-1:0] N_size_i,
-    output logic        [ SRAMAddrWidthA-1:0] sram_a_addr_o,
-    output logic        [ SRAMAddrWidthB-1:0] sram_b_addr_o,
-    output logic        [ SRAMAddrWidthC-1:0] sram_c_addr_o,
-    input  logic signed [     DataWidthA-1:0] sram_a_rdata_i,
-    input  logic signed [     DataWidthB-1:0] sram_b_rdata_i,
-    output logic signed [     DataWidthC-1:0] sram_c_wdata_o,
-    output logic                              sram_c_we_o,
-    output logic                              done_o
+    input  logic                            clk_i,
+    input  logic                            rst_ni,
+    input  logic                            start_i,
+    input  logic        [SizeAddrWidth-1:0] M_size_i,
+    input  logic        [SizeAddrWidth-1:0] K_size_i,
+    input  logic        [SizeAddrWidth-1:0] N_size_i,
+    output logic        [    AddrWidth-1:0] sram_a_addr_o,
+    output logic        [    AddrWidth-1:0] sram_b_addr_o,
+    output logic        [    AddrWidth-1:0] sram_c_addr_o,
+    input  logic signed [  InDataWidth-1:0] sram_a_rdata_i,
+    input  logic signed [  InDataWidth-1:0] sram_b_rdata_i,
+    output logic signed [ OutDataWidth-1:0] sram_c_wdata_o,
+    output logic                            sram_c_we_o,
+    output logic                            done_o
 );
 
-  logic [SystemAddrWidth-1:0] M_count;
-  logic [SystemAddrWidth-1:0] K_count;
-  logic [SystemAddrWidth-1:0] N_count;
+  logic [SizeAddrWidth-1:0] M_count;
+  logic [SizeAddrWidth-1:0] K_count;
+  logic [SizeAddrWidth-1:0] N_count;
 
   logic busy;
   logic valid_data;
   assign valid_data = start_i || busy;  // Always valid in this simple design
 
   gemm_controller #(
-      .AddrWidth      ( SystemAddrWidth )
+      .AddrWidth      ( SizeAddrWidth )
   ) i_gemm_controller (
       .clk_i          ( clk_i       ),
       .rst_ni         ( rst_ni      ),
@@ -70,10 +64,10 @@ module gemm_accelerator_top #(
   end
 
   // The MAC PE instantiation and data path logics
-  mac_pe #(
-      .DataWidthA ( DataWidthA             ),
-      .DataWidthB ( DataWidthB             ),
-      .DataWidthC ( DataWidthC             )
+  general_mac_pe #(
+      .InDataWidth ( InDataWidth             ),
+      .NumInputs   ( 1             ),
+      .OutDataWidth ( OutDataWidth             )
   ) i_mac_pe (
       .clk_i      ( clk_i                  ),
       .rst_ni     ( rst_ni                 ),
@@ -81,7 +75,8 @@ module gemm_accelerator_top #(
       .b_i        ( sram_b_rdata_i         ),
       .a_valid_i  ( valid_data             ),
       .b_valid_i  ( valid_data             ),
-      .acc_clr_i  ( sram_c_we_o || start_i ), // Clear accumulator when writing back
+      .init_save_i( sram_c_we_o || start_i ),
+      .acc_clr_i  ( !busy ), // Clear accumulator when writing back
       .c_o        ( sram_c_wdata_o         )
   );
 
